77cdd1d275c7e05d597d9f02ebc3810ebe9f944d
김유빈 <81549915+bbb2783@users.noreply.github.com>
2023-05-21 18:09:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 from requests.exceptions import RequestException
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 API_ENDPOINT = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0"
 API_KEY = "d0mP886Qs5a3Y4SvZex0oXe%2FDGPx%2BkWUzw8y8sym7k23cQ2tIrgqsiK5TvbotTaODr74xm3rCD5wy899P%2BoTSg%3D%3D"
@@ -57,3 +59,28 @@ if st.button("조회"):
     if weather_data:
         st.subheader(f"{city}의 기상 정보")
         st.table(weather_data)
+        
+        # 차트 출력
+        chart_type = st.selectbox("차트 유형", ["Line Plot", "Bar Plot"])
+        
+        if chart_type == "Line Plot":
+            # Line Plot 생성
+            plt.figure(figsize=(8, 6))
+            for item in weather_data:
+                fcst_time = item['fcstTime']
+                fcst_value = float(item['fcstValue'])
+                plt.plot(fcst_time, fcst_value, marker='o', label=item['category'])
+            plt.xlabel('시간')
+            plt.ylabel('값')
+            plt.legend()
+            st.pyplot(plt)
+        elif chart_type == "Bar Plot":
+            # Bar Plot 생성
+            plt.figure(figsize=(8, 6))
+            categories = [item['category'] for item in weather_data]
+            fcst_values = [float(item['fcstValue']) for item in weather_data]
+            sns.barplot(x=categories, y=fcst_values)
+            plt.xlabel('카테고리')
+            plt.ylabel('값')
+            plt.xticks(rotation=45)
+            st.pyplot(plt)
