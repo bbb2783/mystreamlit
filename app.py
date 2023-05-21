e2847174586917b66c45264d2ff070c1fa854d88
김유빈 <81549915+bbb2783@users.noreply.github.com>
2023-05-21 18:00:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
 import streamlit as st
 import requests
-from requests.exceptions import JSONDecodeError
-import urllib.parse
+from requests.exceptions import RequestException
+import xml.etree.ElementTree as ET
 
 API_ENDPOINT = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0"
 API_KEY = "d0mP886Qs5a3Y4SvZex0oXe%2FDGPx%2BkWUzw8y8sym7k23cQ2tIrgqsiK5TvbotTaODr74xm3rCD5wy899P%2BoTSg%3D%3D"
 
 def fetch_weather_data(city):
-    url = f"{API_ENDPOINT}?serviceKey={API_KEY}&city={city}"
+    url = f"{API_ENDPOINT}/getUltraSrtNcst"
+    params = {
+        'serviceKey': API_KEY,
+        'pageNo': '1',
+        'numOfRows': '10',
+        'dataType': 'XML',
+        'base_date': '20210628',
+        'base_time': '0600',
+        'nx': '55',
+        'ny': '127'
+    }
     
     try:
-        response = requests.get(url)
-        data = response.json()
+        response = requests.get(url, params=params)
+        xml_data = response.content
+        
+        # XML 파싱
+        root = ET.fromstring(xml_data)
+        
+        # 필요한 데이터 추출 및 처리
+        data = extract_data_from_xml(root)
+        
         return data
-    except JSONDecodeError as e:
-        st.error("기상청 API 응답 처리 오류: 올바른 응답 형식이 아닙니다.")
-        st.error(str(e))
+    except RequestException as e:
+        st.error("기상청 API 응답 처리 오류: {}".format(str(e)))
         return None
+
+def extract_data_from_xml(root):
+    # XML 데이터 추출 및 처리하는 로직을 작성해야 함
+    # 필요한 데이터를 추출하여 반환하는 형태로 작성
+    
+    return extracted_data
 
 st.title("기상 정보 조회")
 city = st.text_input("도시 입력", "서울")
